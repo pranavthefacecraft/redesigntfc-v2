85f2c457ed967bf65e4d88645b1257b62b8a8928
Pranav <prasharpranav@gmail.com>
2025-10-06 18:58:18 +0530

    fix coming soon

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Change to '/subdirectory/' if deployed in subdirectory
   plugins: [
     react(),
     tailwindcss(),
