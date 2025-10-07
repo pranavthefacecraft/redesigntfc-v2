54eef0516591fd42663c2d9f98c2b13332b2f2ed
abhicominin <abhijeetkushwah114@gmail.com>
2025-10-07 15:13:16 +0530

    testpage clouds push three

--- src/pages/TrialClouds/fragmentShader.glsl
+++ src/pages/TrialClouds/fragmentShader.glsl
@@ -116,7 +116,7 @@ void main() {
 
   float t = 1.0 - smoothstep(0.0, 1.4, abs(0.1-d));
   vec3 clampedLightColor = clamp(vec3(1.0,0.5,0.3), vec3(0.0), vec3(1.0));
-  color += 0.6 * pow(clampedLightColor, vec3(1.0/2.2)) * pow(t, 2.0);
+  //   color += 0.6 * pow(clampedLightColor, vec3(1.0/2.2)) * pow(t, 2.0);
 
   gl_FragColor = vec4(color, 1.0);
 }
