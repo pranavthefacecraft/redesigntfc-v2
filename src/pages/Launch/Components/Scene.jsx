fa867f3566dac10dd0348393c5eae11227dab05a
abhicominin <abhijeetkushwah114@gmail.com>
2025-10-09 11:33:17 +0530

    test

--- src/pages/Launch/Components/Scene.jsx
+++ src/pages/Launch/Components/Scene.jsx
@@ -1,12 +1,18 @@
-import { useTexture } from "@react-three/drei";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useTexture, useFBO, OrthographicCamera } from "@react-three/drei";
+import { Canvas, useFrame, useThree, createPortal, extend } from "@react-three/fiber";
+import { useControls } from "leva";
 import { useRef, Suspense } from "react";
 import * as THREE from "three";
 import { v4 as uuidv4 } from "uuid";
 
-
+import BicubicUpscaleMaterial from './Clouds/bicubic';
+import getFullscreenTriangle from './Clouds/Utils';
 import vertexShader from "./Clouds/vertexShader.glsl";
 import fragmentShader from "./Clouds/fragmentShader.glsl";
+import cubeVertex from './cubeVertex.glsl'
+import cubeFragment from './cubeFragment.glsl'
+
+extend({ BicubicUpscaleMaterial });
 
 const DPR = 0.5;
 // Blue noise texture
@@ -16,7 +22,38 @@ const NOISE_TEXTURE_URL = "https://cdn.maximeheckel.com/noises/noise2.png";
 
 const Raymarching = () => {
   const mesh = useRef();
-  const { viewport } = useThree();
+  const screenMesh = useRef();
+  const screenCamera = useRef();
+  const planeMesh = useRef();
+  const upscalerMaterialRef = useRef();
+  const { size, viewport } = useThree();
+
+  const magicScene = new THREE.Scene();
+  const newScene = new THREE.Scene();
+
+  const {
+    resolution,
+  } = useControls({
+    resolution: {
+      value: 2,
+      options: {
+        "1x": 1,
+        "0.5x": 2,
+        "0.25x": 4,
+        "0.125x": 8,
+      },
+    },
+  });
+
+  const renderTargetA = useFBO(
+    window.innerWidth / resolution,
+    window.innerHeight / resolution
+  );
+
+  const renderTargetB = useFBO(
+    size.width,
+    size.height
+  );
 
   const blueNoiseTexture = useTexture(BLUE_NOISE_TEXTURE_URL);
   blueNoiseTexture.wrapS = THREE.RepeatWrapping;
@@ -40,35 +77,90 @@ const Raymarching = () => {
     uFrame: new THREE.Uniform(0),
   };
 
+  const planeuniforms = {
+    uTexture: new THREE.Uniform(null),
+  };
+
   useFrame((state) => {
-    const { clock } = state;
+    const { gl, clock, camera } = state;
     mesh.current.material.uniforms.uTime.value = clock.getElapsedTime();
     mesh.current.material.uniforms.uResolution.value = new THREE.Vector2(
-      window.innerWidth * DPR,
-      window.innerHeight * DPR
+      renderTargetA.width,
+      renderTargetA.height
     );
+
     mesh.current.material.uniforms.uBlueNoise.value = blueNoiseTexture;
     mesh.current.material.uniforms.uNoise.value = noisetexture;
 
     mesh.current.material.uniforms.uFrame.value += 1;
+
+    gl.setRenderTarget(renderTargetA);
+    gl.render(magicScene, camera);
+
+    upscalerMaterialRef.current.uniforms.uTexture.value = renderTargetA.texture;
+    screenMesh.current.material = upscalerMaterialRef.current;
+
+    gl.setRenderTarget(renderTargetB);
+    gl.render(newScene, screenCamera.current);
+
+    planeMesh.current.material.uniforms.uTexture.value = renderTargetB.texture;
+
+    gl.setRenderTarget(null);
   });
 
   return (
-    <mesh ref={mesh} scale={[viewport.width, viewport.height, 1]}>
-      <planeGeometry args={[1, 1]} />
-      <shaderMaterial
-        key={uuidv4()}
-        fragmentShader={fragmentShader}
-        vertexShader={vertexShader}
-        uniforms={uniforms}
-      />
-    </mesh>
+    <>
+      {createPortal(
+        <>
+        <OrthographicCamera ref={screenCamera} args={[-1, 1, 1, -1, 0, 1]} />
+        <mesh ref={mesh} scale={[viewport.width, viewport.height, 1]}>
+          <planeGeometry args={[1, 1]} />
+          <shaderMaterial
+            key={uuidv4()}
+            fragmentShader={fragmentShader}
+            vertexShader={vertexShader}
+            uniforms={uniforms}
+            wireframe={false}
+          />
+        </mesh>
+        </>,
+        magicScene
+      )}
+
+     {createPortal(
+        <>
+        <bicubicUpscaleMaterial ref={upscalerMaterialRef} key={uuidv4()} />
+        <mesh
+          ref={screenMesh}
+          geometry={getFullscreenTriangle()}
+          frustumCulled={false}
+        >
+          <meshBasicMaterial />
+        </mesh>
+        </>, newScene
+     )}
+
+
+      <mesh ref={planeMesh} scale={[viewport.width, viewport.height, 1]} position={[0, 0, 0]}>
+        <planeGeometry args={[1, 1]} />
+        <shaderMaterial
+          key={uuidv4()}
+          fragmentShader={cubeFragment}
+          vertexShader={cubeVertex}
+          uniforms={planeuniforms}
+          wireframe={false}
+        />
+      </mesh>
+
+
+
+    </>
   );
 };
 
 const Scene = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 6] }} dpr={DPR}>
+    <Canvas camera={{ position: [0, 0, 6] }} dpr={[1, 2]} className="canvas w-full h-full fixed top-0 left-0">
       <Suspense fallback={null}>
         <Raymarching />
       </Suspense>
