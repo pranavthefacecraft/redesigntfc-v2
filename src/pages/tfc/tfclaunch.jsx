ddff1bb8a8db280bbbac9f2474ffa78f57f80ed2
abhicominin <abhijeetkushwah114@gmail.com>
2025-10-16 21:34:14 +0530

    chatbot

--- src/pages/tfc/tfclaunch.jsx
+++ src/pages/tfc/tfclaunch.jsx
@@ -8,6 +8,7 @@ export default function TFCLaunch() {
         <PlayProvider>
         <div className="tfc-page fixed inset-0 overflow-hidden bg-black"> 
          <div className="mainpage fixed inset-0 overflow-hidden max-w-[1920px] max-h-[1920px] mx-auto my-auto"> 
+           
            <Main />
            <MouseScrollCursor/>
          </div>
