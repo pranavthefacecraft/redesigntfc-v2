44bf54e116fd5f5364614de904438c983aed3bb5
abhicominin <abhijeetkushwah114@gmail.com>
2025-11-10 18:18:47 +0530

    prototype

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <Route path="/Project" element={<Project/>} />
       <Route path="/solit" element={<Solit />} />
       <Route path="/tanda" element={<Tanda />} />
+      <Route path="/tfchome" element={<NewHome/>} />
       <Route path="/testwidget" element={<ComingSoon />} />
       <Route path="/test" element={<Test />} />
       <Route path="/rafw" element={<Rafw />} />
